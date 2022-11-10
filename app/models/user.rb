97c87a0c35f117200c3a18e2d3538a147283d956
EC2 Default User <ec2-user@ip-172-31-8-66.ap-northeast-1.compute.internal>
2022-11-10 08:53:32 +0000

    Set validate for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :posts
+  
+  # validates カラム名(シンボル), 検証のルール
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :age, numericality: true  # 数値のみ許可
+  validates :introduction, length: { in: 10..30 } #10文字以上30文字以内
 end
